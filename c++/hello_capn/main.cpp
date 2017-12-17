305f6e199ca6793c2733397b829f4ed69f28781f
geoffrey <gatkinson@skyloupe.com>
2017-12-17 12:17:52 -0500

    Add HelloCapn.  Some cleanup

--- c++/hello_capn/main.cpp
+++ c++/hello_capn/main.cpp
@@ -26,53 +26,29 @@ SOFTWARE.
 #include <capnp/message.h>
 #include <capnp/compat/json.h>
 
-#include "myproto.capnp.h"
+#include "hello.capnp.h"
 
-void test1()
+void do_sample()
 {
+    //Instantiate a builder
     capnp::MallocMessageBuilder message;
     
-    MyProto::Builder mmbuilder = message.initRoot<MyProto>();
+    Hello::Builder hellobuilder = message.initRoot<Hello>();
+    
+    // Set the field value
+    hellobuilder.setMystring("Hello CapnProto!");
 
-    capnp::List<::capnp::Text>::Builder mypets = mmbuilder.initPets(3);
-
-    mmbuilder.setId(123);
-
-    mmbuilder.setName("Geoffrey");
-
-    mypets.set(0, "Jessie");
-    mypets.set(1, "Jethro");
-    mypets.set(2, "Josey");
-
-/////////////////
-
-    MyProto::Reader mmreader = message.getRoot<MyProto>();
-
-    std::cout << mmreader.getId() << std::endl;
-    std::cout << mmreader.getName().cStr() << std::endl;
-
-    for (int i = 0; i < mmreader.getPets().size(); i++)
-    {
-        std::cout << mmreader.getPets()[i].cStr() << std::endl;
-    }
-
-/////////////////
-
-    capnp::JsonCodec jscodec;
-
-    jscodec.setPrettyPrint(true);
-
-    kj::String kjs = jscodec.encode(mmreader);
-
-    std::cout << kjs.cStr() << std::endl;
+    //Instantiate a reader
+    Hello::Reader helloreader = message.getRoot<Hello>();
+    
+    //Print out the field value
+    std::cout << helloreader.getMystring().cStr() << std::endl;
 }
 
 
 int main(int argc, const char* argv[])
 {
-	std::cout << "A small CapnProto test application" << std::endl;
+    do_sample();
 
-	test1();
-
-	return 0;
+    return 0;
 }
